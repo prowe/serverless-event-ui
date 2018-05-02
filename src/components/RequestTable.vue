6069b8d54a495c2efd9afd96974b99e0a2be8f11
Paul Rowe <prowe@sourceallies.com>
2018-05-01 20:40:27 -0500

    initial commit

---
+++ src/components/RequestTable.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <table>
+        <tbody>
+            <tr v-for="r in requests" :key="r.id">
+                <td>{{r.status}}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: 'RequestTable',
+        data() {
+            return {
+                requests: [
+                    {
+                        id: 'a',
+                        status: 'A'
+                    },
+                    {
+                        id: 'b',
+                        status: 'B'
+                    }
+                ]
+            };
+        },
+
+    }
+</script>
